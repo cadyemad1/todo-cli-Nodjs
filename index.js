5dac84562497455e40032896fd3d0a6ce56316e6
cadyemad1 <cady.emad@stud.fci-cu.edu.eg>
2020-04-16 16:20:37 +0200

    bonus task

--- index.js
+++ index.js
@@ -12,17 +12,19 @@ program
 program
   .command("list")
   .description("list all to-dos")
-  .action(() => {
-    list();
+  .option("-s, --status <status>")
+  .action(({ status }) => {
+    list(status);
   });
 
 program
   .command("edit")
   .description("Edit todo with id")
   .option("-t,--title <title>")
+  .option("-s,--status <status>")
   .option("-i, --id <id>")
-  .action(({ title, id }) => {
-    edit(title, id);
+  .action(({ title, id, status }) => {
+    edit(title, id, status);
   });
 
 program
@@ -33,41 +35,55 @@ program
     Delete(source);
   });
 
-const readFile = () => JSON.parse(fs.readFileSync("./db.json") || "[]");
+const statusValue = ["todo", "progress", "done"];
+const readFile = () => JSON.parse(fs.readFileSync("./db.json", "utf8") || "[]");
 const writeFile = todos => fs.writeFileSync("./db.json", JSON.stringify(todos));
 
 const add = title => {
   //read array
   let todos = readFile();
   //push into array
-  let id = todos.length + 1;
-  todos.push({ id, title });
+  let id = todos.length === 0 ? 1 : todos[todos.length - 1].id + 1;
+  todos.push({ id, title, status: "todo" });
   //save
   writeFile(todos);
 };
 
-const list = () => {
+const list = status => {
   //read array
   let todos = readFile();
-  console.table(todos);
+  if (status) {
+    let filteredTodos = todos.filter(t => t.status === status);
+    console.table(filteredTodos);
+  } else console.table(todos);
 };
 
-const edit = (title, id) => {
+const edit = (title, id, status) => {
   let todos = readFile();
   let todoEditedId = todos.findIndex(t => t.id == id);
-  todos[todoEditedId] = { id, title };
+  let statusCorrect;
+
+  if (status !== undefined)
+    statusCorrect = statusValue.includes(status.toLowerCase());
+
+  title ? (todos[todoEditedId] = { ...todos[todoEditedId], title }) : "";
+
+  statusCorrect
+    ? (todos[todoEditedId] = {
+        ...todos[todoEditedId],
+        status: status.toLowerCase()
+      })
+    : "";
+
   writeFile(todos);
 };
 
 const Delete = id => {
-  console.log("id is", id);
-
   let todos = readFile();
+
   let todoId = todos.findIndex(t => t.id == id);
-  console.log(todoId);
 
   todos.splice(todoId, 1);
-  console.log(todos);
 
   writeFile(todos);
 };
